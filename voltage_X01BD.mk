3bcbcc280c91e7b1d6699de54a0396860adb0ea4
khirendra03 <khirendrapatel3@gmail.com>
2021-12-12 12:49:00 +0530

    X01BD: Go Official

--- voltage_X01BD.mk
+++ voltage_X01BD.mk
@@ -15,6 +15,9 @@ $(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 2160
 
+# Voltage Build Type
+VOLTAGE_BUILD_TYPE := OFFICIAL
+
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
