7f5d12e593fa634bf1f57818fb6af4fb3b1a9480
khirendra03 <khirendrapatel3@gmail.com>
2022-01-30 09:36:57 +0530

    X01BD: VoltageOS bringup

--- voltage_X01BD.mk
+++ voltage_X01BD.mk
@@ -9,16 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common voltageOS stuff
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-DEVICE_MAINTAINER := SonalSingh
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 2160
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_X01BD
+PRODUCT_NAME := voltage_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Asus Zenfone Max Pro M2
