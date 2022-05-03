db19c74bf31664d87d99153906cbcc9bcd5adaf0
khirendra03 <khirendrapatel3@gmail.com>
2022-05-03 12:11:09 +0530

    X01BD: Updated to May Raven FP

--- voltage_X01BD.mk
+++ voltage_X01BD.mk
@@ -36,7 +36,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X01BD_1 \
     PRODUCT_NAME=WW_X01BD \
-    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.004 8233519 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220505.002 8353555 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT="google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys"
+BUILD_FINGERPRINT="google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys"
