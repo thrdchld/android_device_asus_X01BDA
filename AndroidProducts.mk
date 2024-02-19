3ee12e34c419add1630c4f6c6c1fb5675ce9b4e9
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-02-19 17:07:06 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_X01BD.mk
+    $(LOCAL_DIR)/lineage_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_X01BD-eng \
-        evolution_X01BD-user \
-        evolution_X01BD-userdebug
+    lineage_X01BD-eng \
+        lineage_X01BD-user \
+        lineage_X01BD-userdebug
