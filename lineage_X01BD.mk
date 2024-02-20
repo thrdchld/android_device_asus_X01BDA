53de0a4532d99e7ef0e053ac87ceeeb523cd0797
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-02-20 07:29:12 +0530

    Update lineage_X01BD.mk

--- lineage_X01BD.mk
+++ lineage_X01BD.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common lineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
