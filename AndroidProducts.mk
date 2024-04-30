ee6ee6143f67b98657d9bde70fc1beec00d693a1
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-30 13:00:42 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_X01BD.mk
+    $(LOCAL_DIR)/voltage_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_X01BD-eng \
-        evolution_X01BD-user \
-        evolution_X01BD-userdebug
+    voltage_X01BD-eng \
+        voltage_X01BD-user \
+        voltage_X01BD-userdebug
