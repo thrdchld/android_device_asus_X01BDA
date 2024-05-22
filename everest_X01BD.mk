837f9e8105a9d881f261b93e9b81073e915ad004
thrdchld <109644096+thrdchld@users.noreply.github.com>
2024-05-22 22:42:07 +0700

    Update and rename lineage_X01BD.mk to everest_X01BD.mk

--- everest_X01BD.mk
+++ everest_X01BD.mk
@@ -10,16 +10,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/everest/config/common_full_phone.mk)
 
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-MATRIXX_MAINTAINER := ManitnjG
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X01BD
+PRODUCT_NAME := everest_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_X01BDA
