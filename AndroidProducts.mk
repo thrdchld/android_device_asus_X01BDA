c96689e0f1912fdeb0a00325c0ba2cc0d9cb996a
thrdchld <109644096+thrdchld@users.noreply.github.com>
2024-06-04 22:16:22 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/everest_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-eng \
+        everest_X01BD-ap1a-eng \
         everest_X01BD-ap1a-user \
         everest_X01BD-ap1a-userdebug
